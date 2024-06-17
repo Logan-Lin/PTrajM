7c5fe867ba9e76300976d6e0905f8a3b629080e0
yichenliuzong <19231173@bjtu.edu.cn>
2024-06-17 13:39:41 +0800

    Add evaluation for prediction

---
+++ predict_eval.py
@@ -0,0 +1,50 @@
+import os
+import json
+import numpy as np
+from argparse import ArgumentParser
+from functools import partial
+from data import X_COL, Y_COL, DT_COL, ROAD_COL
+from utils import cal_classification_metric, cal_regression_metric, cal_distance_metric
+
+PRED_SAVE_DIR = os.environ.get('PRED_SAVE_DIR', 'predictions')
+
+parser = ArgumentParser()
+parser.add_argument('-s', '--settings', help='name of the settings file to use', type=str, default="chengdu_ft_gps") # required=True
+args = parser.parse_args()
+
+# Load the settings file, and save a backup in the cache directory.
+with open(os.path.join('settings', f'{args.settings}.json'), 'r') as fp:
+    settings = json.load(fp)
+
+# Iterate through the multiple settings.
+for setting_i, setting in enumerate(settings):
+    print(f'===SETTING {setting_i}/{len(settings)}===')
+    SAVE_NAME = setting.get('save_name', None)
+    
+    if 'test' in setting:
+        test_setting = setting['test']
+        if test_setting.get('save', False):
+            predictions = np.load(os.path.join(PRED_SAVE_DIR, SAVE_NAME, 'predictions.npy'))
+            targets = np.load(os.path.join(PRED_SAVE_DIR, SAVE_NAME, 'targets.npy'))
+            print(f"predictions: {predictions.shape}")
+            print(f"targets: {targets.shape}")
+            # print(predictions[0])
+            # print(targets[0])
+            
+            pred_cols = test_setting['padder']['params']["pred_cols"] # list
+            if pred_cols == [ROAD_COL]: # 预测路段id，是分类任务
+                cal_metric = cal_classification_metric
+                metric_filename = "road_classification"
+            elif sorted(pred_cols) == sorted([Y_COL, X_COL]): # 预测'lng''lat'
+                lng_col, lat_col = pred_cols.index(X_COL), pred_cols.index(Y_COL)
+                print(f"lng_col: {lng_col}, lat_col: {lat_col}")
+                cal_metric = partial(cal_distance_metric, lng_col=lng_col, lat_col=lat_col)
+                metric_filename = "gps_regression"
+            else: # 预测'delta_t' （预测'lng''lat'）
+                cal_metric = cal_regression_metric
+                metric_filename = "delta_t_regression"
+                
+            metric = cal_metric(targets, predictions)
+            print(f"the test metric for {pred_cols}:")
+            print(metric)
+            metric.to_hdf(os.path.join(PRED_SAVE_DIR, SAVE_NAME, f'{metric_filename}.h5'), key='metric', format='table')
--- utils.py
+++ utils.py
@@ -3,8 +3,10 @@ import string
 import random
 import math
 import numpy as np
+import pandas as pd
 import torch
 from datetime import datetime
+from sklearn.metrics import mean_squared_error, mean_absolute_error, f1_score, recall_score, accuracy_score, roc_auc_score
 
 
 def get_datetime_key():
@@ -75,3 +77,98 @@ class DotDict(dict):
         if isinstance(value, dict):
             value = DotDict(value)
         return value
+    
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    """ Calculcates the MAPE metric. """
+    mape = np.mean(np.abs((y_true - y_pred) / y_true))
+    return mape
+
+def cal_regression_metric(label, pres):
+    """ Calculcate all common regression metrics. """
+    rmse = math.sqrt(mean_squared_error(label, pres))
+    mae = mean_absolute_error(label, pres)
+    mape = mean_absolute_percentage_error(label, pres)
+
+    s = pd.Series([rmse, mae, mape], index=['rmse', 'mae', 'mape'])
+    return s
+
+
+def distance_mae(distance, null_val=np.nan):
+    distance_mae = np.mean(np.abs(distance))
+    return distance_mae
+    # if np.isnan(null_val):
+    #     mask = ~torch.isnan(distance)
+    # else:
+    #     mask = (distance!=null_val)
+    # mask = mask.float()
+    # mask /=  np.mean((mask))
+    # mask = np.where(np.isnan(mask), np.zeros_like(mask), mask)
+    # # loss = torch.abs(preds-labels)
+    # loss = np.abs(distance)
+    # loss = loss * mask
+    # loss = np.where(np.isnan(loss), np.zeros_like(loss), loss)
+    # return np.mean(loss)
+
+def distance_mse(distance, null_val=np.nan):
+    distance_mse = np.mean(distance**2)
+    return distance_mse
+    # if np.isnan(null_val):
+    #     mask = ~torch.isnan(distance)
+    # else:
+    #     mask = (distance!=null_val)
+    # mask = mask.float()
+    # mask /= np.mean((mask))
+    # mask = np.where(np.isnan(mask), np.zeros_like(mask), mask)
+    # # loss = (preds-labels)**2
+    # loss = distance**2
+    # loss = loss * mask
+    # loss = np.where(np.isnan(loss), np.zeros_like(loss), loss)
+    # return np.mean(loss)
+
+def distance_rmse(distance, null_val=np.nan):
+    return np.sqrt(distance_mse(distance=distance, null_val=null_val))
+
+def cal_distance_metric(label, pres, lng_col, lat_col):
+    """ 
+    Calculcate all distance regression metrics. 
+
+    :param labels: longitude and latitude features of the trajectories, with shape (B,2).
+    :param pres: predicted longitude and latitude features of the trajectories, with shape (B, 2).
+    """
+    distance = cal_geo_distance(label[...,lng_col], label[...,lat_col], pres[...,lng_col], pres[...,lat_col])
+    mae = distance_mae(distance, 0.0)#.item()
+    rmse = distance_rmse(distance, 0.0)#.item()
+    s = pd.Series([rmse, mae], index=['distance_rmse', 'distance_mae'])
+    return s
+
+
+def top_n_accuracy(truths, preds, n):
+    """ Calculcate Acc@N metric. """
+    # best_n = np.argsort(preds, axis=1)[:, -n:] # 升序排列求后n个
+    best_n = np.argsort(-preds, axis=1)[:, :n] # 降序排列求前n个
+    successes = 0
+    for i, truth in enumerate(truths):
+        if truth in best_n[i, :]:
+            successes += 1
+    return float(successes) / truths.shape[0]
+
+
+def cal_classification_metric(labels, pres):
+    """
+    Calculates all common classification metrics.
+
+    :param labels: classification label, with shape (N).
+    :param pres: predicted classification distribution, with shape (N, num_class).
+    """
+    pres_index = pres.argmax(-1)  # (N)
+    macro_f1 = f1_score(labels, pres_index, average='macro', zero_division=0)
+    macro_recall = recall_score(labels, pres_index, average='macro', zero_division=0)
+    acc = accuracy_score(labels, pres_index)
+    n_list = [5, 10]
+    top_n_acc = [top_n_accuracy(labels, pres, n) for n in n_list]
+
+    s = pd.Series([macro_f1, macro_recall, acc] + top_n_acc,
+                  index=['macro_f1', 'macro_rec'] +
+                  [f'acc@{n}' for n in [1] + n_list])
+    return s
